ac05aecfe4b3d501c2fc2d3b192e556ad50885cf
Meghna Tiwari <meghnatw24@gmail.com>
2025-12-12 00:19:58 +0530

    Comments

--- phishing_ai.py
+++ phishing_ai.py
@@ -1,15 +1,15 @@
-from transformers import pipeline
+from transformers import pipeline  #Transformers is a hugging face library for pretrained NLP models, pipeline is a high level API
 
 # Load pretrained phishing/spam detection model
 phishing_detector = pipeline(
-    "text-classification",
+    "text-classification", #sets up text calssification task
     model="mrm8488/bert-tiny-finetuned-sms-spam-detection"
 )
-def detect_phishing(text):
-    result = phishing_detector(text)[0]
+def detect_phishing(text): #input:text (string to analyze)
+    result = phishing_detector(text)[0] #runs the model, returns a lsit of predictions ,[0] takes the first prediction
 
     return{
-        "label" : result["label"],
-        "confidence": round(result["score"] * 100,2)
+        "label" : result["label"], #"label" is predicted class
+        "confidence": round(result["score"] * 100,2) #model's confidence score, converted to a precentage and rounded to 2 decimals
     }
 
